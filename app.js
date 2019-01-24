9e980126fa81ab402dbdd9a7ea70900b7c04025b
Fong <fong@videodock.com>
2019-01-24 09:58:37 +0100

    chore(database): setup DB

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+
+mongoose.connect('mongodb://fong1980:dejavu1@ds229909.mlab.com:29909/gql-ninja')
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
+
+console.log('________',process.env.DBNAME_PASSWORD)
 
 app.use('/graphql', graphqlHTTP({
     schema,
